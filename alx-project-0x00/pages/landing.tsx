0bd083e9049262f44a48e9daef4eef4ea9a7ec10
ephrim-s <ephrims5@gmial.com>
2025-07-20 06:20:25 +0000

    updated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,13 +1,19 @@
+import React from "react";
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
         <h1 className="text-xl font-extralight">Landing Page</h1>
+        <div className="flex gap-4 flex-wrap">          
+          <Button title="Small Button" styles="text-sm rounded-sm" />
+          <Button title="Medium Button" styles="text-base rounded-md" />
+          <Button title="Large Button" styles="text-lg rounded-full" />
+        </div>
         <Card />
-         <Card />
-          <Card />
-           <Card />
+
+
     </div>
   );
 }

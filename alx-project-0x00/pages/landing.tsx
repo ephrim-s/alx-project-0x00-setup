5c8c5c65c53d67f9d776d22100bccf18e7a6a803
ephrim-s <ephrims5@gmial.com>
2025-07-20 06:21:56 +0000

    updated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,7 +9,7 @@ const Landing: React.FC = () => {
         <div className="flex gap-4 flex-wrap">          
           <Button title="Small Button" styles="text-sm rounded-sm" />
           <Button title="Medium Button" styles="text-base rounded-md" />
-          <Button title="Large Button" styles="text-lg rounded-full" />
+          <Button title="Large Button" styles="text-lg rounded-lg" />
         </div>
         <Card />
 
